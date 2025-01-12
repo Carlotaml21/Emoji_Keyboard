3e01bbec017c62a986ded56e43e9a2fb4daaa2f0
Carlotaml21 <carlotaml21@gmail.com>
2025-01-12 13:21:50 +0100

    add js script funcionality

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+const divs = document.querySelectorAll('.FlexContainer div');
+const texts = ["Dog", "Cat", "Rabbit", "Mouse", "Sheep", "Pig", "Cow", "Horse", "Duck", "Snake", "Fish", "Squirrel", "Swan", "Raven"];
+const emojis = ['🐕', '🐈', '🐇', '🐁', '🐑', '🐖', '🐄', '🐎', '🦆', '🐍', '🐟', '🐿️', '🦢​​', '🐦‍⬛'];
+
+let index = 0;
+let showingText = false;
+
+function toggleEmojiAndText() {
+  const currentDiv = divs[index];
+  
+  if (showingText) {
+    currentDiv.textContent = emojis[index];
+    currentDiv.style.fontSize = "50px";
+  } else {
+    currentDiv.textContent = texts[index];
+    currentDiv.style.fontSize = "20px";
+    currentDiv.style.color = "Black";
+    currentDiv.style.fontFamily = "'Gill Sans', sans-serif";
+  }
+
+  showingText = !showingText;
+
+  if (!showingText) {
+    index = (index + 1) % divs.length;
+  }
+
+  setTimeout(toggleEmojiAndText, 1000);
+}
+
+toggleEmojiAndText();
